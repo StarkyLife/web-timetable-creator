c7c4ca26a7f6624da5f6ceacb0224edafd477efb
Исмагилов Ильшат Фархатович <IIsmagilov@alfabank.ru>
2021-05-26 19:47:01 +0300

    feat(*): add story tests

--- cypress/integration/app-open.feature
+++
@@ -1,4 +0,0 @@
-Feature: Open app
-    Scenario: Open app
-        When go to app page
-        Then see the title
---
+++ cypress/integration/fill-subjects/index.ts
@@ -0,0 +1,28 @@
+import {
+    defineStep,
+    Given,
+    Then,
+    When,
+} from 'cypress-cucumber-preprocessor/steps';
+
+Given('пользователь открыл пустое расписание', () => {
+    throw new Error('Not implemented!');
+});
+
+defineStep('(он )добавит/добавил предмет c названием {string}', () => {
+    throw new Error('Not implemented!');
+});
+
+When('он сохранит и переоткроет текущее расписание', () => {
+    throw new Error('Not implemented!');
+});
+When('он удалит предмет c названием {string}', () => {
+    throw new Error('Not implemented!');
+});
+
+Then('{string} должна быть в списке предметов', () => {
+    throw new Error('Not implemented!');
+});
+Then('{string} должна отсутствовать в списке предметов', () => {
+    throw new Error('Not implemented!');
+});
